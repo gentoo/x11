5a0fe3df2f6a5a7caa95c3927a61708bec5b0e38
Rémi Cardona <remi@gentoo.org>
2009-10-06 01:05:23 +0200

    fix digestcheck script, now it works thanks to Zac

--- scripts/digestcheck.py
+++ scripts/digestcheck.py
@@ -7,6 +7,6 @@ os.environ['PORTDIR'] = os.path.dirname(os.path.dirname(manifest_dir))
 settings = portage.config(clone=portage.settings)
 for directory in sys.argv[1:]:
 	settings['O'] = directory
-	if not portage.digestcheck([], settings):
+	if not portage.digestcheck([], settings, strict=True):
 		sys.exit(1)
 sys.exit(0)
