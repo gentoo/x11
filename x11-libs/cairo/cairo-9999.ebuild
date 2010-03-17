gentoo/x11

commit 9ca2d7bf7b6d6f49ccdd749993cf882f8ab70644

[x11-libs/cairo] Sync with main tree and polish a bit.

x11-libs/cairo/Manifest
@@ -1,3 +1,5 @@
 AUX cairo-1.2.4-lcd-cleartype-like.diff 7244 RMD160 e50d5bf17cbead688a8b808a78fe4104c09af52e SHA1 f63131fd75bb496db59e3ce6bc755bbfb70baacb SHA256 4926d20d2b6b5071a5bf9ee9a12a36fef69510ebebd717c1dab151fe650f173a
-EBUILD cairo-9999.ebuild 2920 RMD160 37865c7187a9904109098f9fb79ee9558fd04fef SHA1 cde52632927ade1a2c7fa0a423d1d9d178e60799 SHA256 af5309a02648c704874477de674cad0b4dec76c48b969b13a2d01c670274d50a
+AUX cairo-1.8.8-interix.patch 619 RMD160 9bb28c5a88aa1f2776d32ac7020aef4ba93872b5 SHA1 857a4d2be0118a6d2d0d80388b5963a5f768341d SHA256 505be82a1a0b7f963cb49ff85ecf1fa1389a4e43621df76b91fc1529616b2299
+AUX cairo-1.8.8-libpng14.patch 461 RMD160 e6676e89f64ae2c1390315ad58d0c697acf66e9d SHA1 0a900ceb2b5fa9721687dc670a1b582fa6c59341 SHA256 d4391514334809b6a13c644d7655c02e5d1070d61653452ff93fcc44bf904fb6
+EBUILD cairo-9999.ebuild 3381 RMD160 c53addfff564c7eab63c0ced6fca120a1163a660 SHA1 5e63ac8a2484ce91e2528c566d7c663ec7a645c2 SHA256 359bb23139046089e85d0768307df43c052fad6d54c9c31b8a741db47759f8c8
 MISC metadata.xml 579 RMD160 a8e2ab1e575154d20f372f4cef09cfee18c6e8c3 SHA1 3f2062d658c0d780083e366f7f3d63d95586d7e1 SHA256 484df9f3a5c0775495ad4022f142f8d1a324abe0377269b8b3696a8f733989e4

x11-libs/cairo/cairo-9999.ebuild
@@ -1,26 +1,30 @@
-# Copyright 1999-2009 Gentoo Foundation
+# Copyright 1999-2010 Gentoo Foundation
 # Distributed under the terms of the GNU General Public License v2
 # $Header: $
 
-inherit eutils flag-o-matic libtool git autotools
+EAPI=2
 
-EGIT_REPO_URI="git://anongit.freedesktop.org/git/cairo"
+if [[ ${PV} == *9999 ]]; then
+	EGIT_REPO_URI="git://anongit.freedesktop.org/git/cairo"
+	GIT_ECLASS="git"
+fi
+
+inherit eutils flag-o-matic autotools ${GIT_ECLASS}
 
 DESCRIPTION="A vector graphics library with cross-device output support"
 HOMEPAGE="http://cairographics.org/"
-SRC_URI=""
+[[ ${PV} == *9999 ]] || SRC_URI="http://cairographics.org/releases/${P}.tar.gz"
 
 LICENSE="|| ( LGPL-2.1 MPL-1.1 )"
 SLOT="0"
-KEYWORDS="~alpha ~amd64 ~arm ~hppa ~ia64 ~mips ~ppc ~ppc64 ~s390 ~sh ~sparc ~x86 ~x86-fbsd"
-IUSE="cleartype debug directfb doc glitz opengl svg X xcb"
+KEYWORDS="alpha amd64 arm hppa ia64 ~mips ppc ppc64 s390 sh sparc x86 ~x86-fbsd ~x86-freebsd ~x86-interix ~amd64-linux ~x86-linux ~ppc-macos ~x64-macos ~x86-macos ~sparc-solaris ~sparc64-solaris ~x64-solaris ~x86-solaris"
+IUSE="aqua cleartype debug directfb doc glitz opengl +svg X xcb"
 
 # Test causes a circular depend on gtk+... since gtk+ needs cairo but test needs gtk+ so we need to block it
 RESTRICT="test"
 
 RDEPEND="media-libs/fontconfig
 	>=media-libs/freetype-2.1.9
-	media-libs/libpng
 	sys-libs/zlib
 	media-libs/libpng
 	>=x11-libs/pixman-0.12.0
@@ -41,41 +45,39 @@ RDEPEND="media-libs/fontconfig
 #	pdf test
 #	x11-libs/pango
 #	>=x11-libs/gtk+-2.0
-#	>=app-text/poppler-bindings-0.9.2
+#	>=app-text/poppler-bindings-0.9.2[gtk]
 #	ps test
-#	virtual/ghostscript
+#	app-text/ghostscript-gpl
 #	svg test
 #	>=x11-libs/gtk+-2.0
 #	>=gnome-base/librsvg-2.15.0
 
 DEPEND="${RDEPEND}
 	>=dev-util/pkgconfig-0.19
+	>=sys-devel/libtool-2
 	doc? (
 		>=dev-util/gtk-doc-1.6
 		~app-text/docbook-xml-dtd-4.2
 	)
-	X? ( x11-proto/renderproto )
-	xcb? ( x11-proto/xcb-proto )"
+	X? ( x11-proto/renderproto )"
 
-src_unpack() {
-	git_src_unpack
-	cd "${S}"
-
+src_prepare() {
 	# ClearType-like patches applied by ArchLinux
-	use cleartype && epatch "${FILESDIR}"/cairo-1.2.4-lcd-cleartype-like.diff
+	use cleartype && epatch "${FILESDIR}"/${PN}-1.2.4-lcd-cleartype-like.diff
+
+	epatch "${FILESDIR}"/${PN}-1.8.8-interix.patch \
+		"${FILESDIR}"/${PN}-1.8.8-libpng14.patch
 
 	# We need to run elibtoolize to ensure correct so versioning on FreeBSD
-	elibtoolize
-
-	# create dummy */Makefile.am.features and ChangeLog to make automake happy
-	> boilerplate/Makefile.am.features
-	> src/Makefile.am.features
-	touch ChangeLog
-
+	# upgraded to an eautoreconf for the above interix patch.
 	eautoreconf
 }
 
-src_compile() {
+src_configure() {
+	[[ ${CHOST} == *-interix* ]] && append-flags -D_REENTRANT
+	# http://bugs.freedesktop.org/show_bug.cgi?id=15463
+	[[ ${CHOST} == *-solaris* ]] && append-flags -D_POSIX_PTHREAD_SEMANTICS
+
 	#gets rid of fbmmx.c inlining warnings
 	append-flags -finline-limit=1200
 
@@ -83,19 +85,27 @@ src_compile() {
 		export glitz_LIBS=$(pkg-config --libs glitz-glx)
 	fi
 
-	econf $(use_enable X xlib) $(use_enable doc gtk-doc) \
-		$(use_enable directfb) $(use_enable xcb) \
-		$(use_enable svg) $(use_enable glitz) $(use_enable X xlib-xrender) \
-		$(use_enable debug test-surfaces) --enable-pdf  --enable-png \
-		--enable-ft --enable-ps \
-		|| die "configure failed"
-
-	emake || die "compile failed"
+	econf \
+		$(use_enable X xlib) \
+		$(use_enable X xlib-xrender) \
+		$(use_enable aqua quartz) \
+		$(use_enable aqua quartz-image) \
+		$(use_enable debug test-surfaces) \
+		$(use_enable directfb) \
+		$(use_enable doc gtk-doc) \
+		$(use_enable glitz) \
+		$(use_enable xcb) \
+		$(use_enable svg) \
+		--enable-pdf \
+		--enable-png \
+		--enable-ft \
+		--enable-ps
 }
 
 src_install() {
-	emake -j1 DESTDIR="${D}" install || die "Installation failed"
-	dodoc AUTHORS ChangeLog NEWS README
+	# parallel make install fails
+	make DESTDIR="${D}" install || die "Installation failed"
+	dodoc AUTHORS ChangeLog NEWS README || die
 }
 
 pkg_postinst() {

x11-libs/cairo/files/cairo-1.8.8-interix.patch
@@ -1,0 +1,16 @@
+diff -ru cairo-1.8.8.orig/build/configure.ac.tools cairo-1.8.8/build/configure.ac.tools
+--- cairo-1.8.8.orig/build/configure.ac.tools	2009-09-30 13:36:42 +0200
++++ cairo-1.8.8/build/configure.ac.tools	2009-09-30 13:50:50 +0200
+@@ -21,5 +21,12 @@
+ *) PKGCONFIG_REQUIRES="Requires.private"; ;;
+ esac
+ 
++dnl hmm... on interix, things go really bad with Requires.private, since libpng12
++dnl is missing on the final link commands, so gtk+'s configure checks for cairo
++dnl fail miserably with unresolved symbols to it.
++case "$host_os" in
++interix*) PKGCONFIG_REQUIRES="Requires" ;;
++esac
++
+ AC_SUBST(PKGCONFIG_REQUIRES)
+ 

x11-libs/cairo/files/cairo-1.8.8-libpng14.patch
@@ -1,0 +1,13 @@
+http://bugs.gentoo.org/show_bug.cgi?id=305117
+
+--- configure.ac
++++ configure.ac
+@@ -161,7 +161,7 @@
+   AC_ARG_VAR([png_REQUIRES], [module name for libpng to search for using pkg-config])
+   if test "x$png_REQUIRES" = x; then
+     # libpng13 is GnuWin32's libpng-1.2.8 :-(
+-    for l in libpng12 libpng13 libpng10 ; do
++    for l in libpng14 libpng12 libpng13 libpng10 ; do
+       if $PKG_CONFIG --exists $l ; then
+         png_REQUIRES=$l
+         use_png=yes

commit f71ec257bdf66f5e344e207bba9bd90ae239f7be

[x11-libs/cairo] Update libpng patch to actually apply against git
GIT got the libpng check modified to also check for "libpng" package like
last summer. So the 1.8.8 patch could have never worked really.
Modified it to apply cleanly - untested runtime though.

This was broken by scarabeus in commit 9ca2d7bf7 and reported by belak on IRC

x11-libs/cairo/Manifest
@@ -1,4 +1,4 @@
 AUX cairo-1.8.8-interix.patch 619 RMD160 9bb28c5a88aa1f2776d32ac7020aef4ba93872b5 SHA1 857a4d2be0118a6d2d0d80388b5963a5f768341d SHA256 505be82a1a0b7f963cb49ff85ecf1fa1389a4e43621df76b91fc1529616b2299
-AUX cairo-1.8.8-libpng14.patch 461 RMD160 e6676e89f64ae2c1390315ad58d0c697acf66e9d SHA1 0a900ceb2b5fa9721687dc670a1b582fa6c59341 SHA256 d4391514334809b6a13c644d7655c02e5d1070d61653452ff93fcc44bf904fb6
-EBUILD cairo-9999.ebuild 3248 RMD160 d2539e22521af2aa7e2e6f4e4e823c2d8ce517a5 SHA1 2c417289cb1492411879105919990f5d3331417e SHA256 3465ebf4be7b3a8f85a87ea24cebe03738268bff56c06135dd1a297814d3932f
+AUX cairo-9999-libpng14.patch 473 RMD160 84dd09ef5fd1c76274c66ec59600d1670f976aea SHA1 acf4137e4a9ec714849b2ef334584483c53c8c82 SHA256 da7f5c54eb0c656b982da0feca3d8569dc6a4c2ffd5a10dd58bd859a281f9394
+EBUILD cairo-9999.ebuild 3241 RMD160 48fa74a348b09b1f0a42232b2e90d6cfd9672980 SHA1 f8da0a8650868f656a34b050e718bafe08fe5bf4 SHA256 f3273d4d6c988322d471c7760ddf37523d79681ac5c4522983ce9dcf424ab341
 MISC metadata.xml 579 RMD160 a8e2ab1e575154d20f372f4cef09cfee18c6e8c3 SHA1 3f2062d658c0d780083e366f7f3d63d95586d7e1 SHA256 484df9f3a5c0775495ad4022f142f8d1a324abe0377269b8b3696a8f733989e4

x11-libs/cairo/cairo-9999.ebuild
@@ -63,7 +63,7 @@ DEPEND="${RDEPEND}
 
 src_prepare() {
 	epatch "${FILESDIR}"/${PN}-1.8.8-interix.patch \
-		"${FILESDIR}"/${PN}-1.8.8-libpng14.patch
+		"${FILESDIR}"/${P}-libpng14.patch
 
 	# We need to run elibtoolize to ensure correct so versioning on FreeBSD
 	# upgraded to an eautoreconf for the above interix patch.

x11-libs/cairo/files/cairo-9999-libpng14.patch
@@ -6,8 +6,8 @@ http://bugs.gentoo.org/show_bug.cgi?id=305117
    AC_ARG_VAR([png_REQUIRES], [module name for libpng to search for using pkg-config])
    if test "x$png_REQUIRES" = x; then
      # libpng13 is GnuWin32's libpng-1.2.8 :-(
--    for l in libpng12 libpng13 libpng10 ; do
-+    for l in libpng14 libpng12 libpng13 libpng10 ; do
+-    for l in libpng12 libpng13 libpng10 libpng; do
++    for l in libpng14 libpng12 libpng13 libpng10 libpng; do
        if $PKG_CONFIG --exists $l ; then
          png_REQUIRES=$l
          use_png=yes